d137245023d53f7583315ef03788e793422a44ef
Py-zante <paizante.guilherme@gmail.com>
2025-07-06 22:40:20 -0300

    adaptação do frontend para o django, templates e static files

--- galeria/views.py
+++ galeria/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 # Create your views here.
 
 def index(request):
-    return HttpResponse('<H1>Hello World<H1>')
+    return render(request, 'galeria/index.html')
+
+def imagem(request):
+        return render(request, 'galeria/imagem.html')
